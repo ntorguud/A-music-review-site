6c583ab32cb4ebb9510c6147480e24c68e81814f
Naran Torguud <ntorguud@gmail.com>
2021-06-18 16:31:53 -0700

    Create first view, base.html, templates folder

--- music/views.py
+++ music/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-
+from 
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def index(request):
+    return render(request, 'music/index.html')
+    
